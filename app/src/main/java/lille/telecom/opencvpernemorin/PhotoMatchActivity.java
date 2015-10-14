9461f64248ad25a0cf348fd1c21880675a06c7f4
tonyop62 <tonyop62@live.fr>
2015-10-14 08:47:13 +0200

    stockage en json

--- app/src/main/java/lille/telecom/opencvpernemorin/PhotoMatchActivity.java
+++ app/src/main/java/lille/telecom/opencvpernemorin/PhotoMatchActivity.java
@@ -17,6 +17,8 @@ import android.widget.Toast;
 
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfDMatch;
 import org.opencv.core.MatOfKeyPoint;
@@ -25,6 +27,7 @@ import org.opencv.features2d.DescriptorMatcher;
 import org.opencv.features2d.FeatureDetector;
 import org.opencv.highgui.Highgui;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.features2d.DMatch;
 
 import android.widget.Toast;
 
@@ -34,10 +37,13 @@ import com.google.gson.JsonParser;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class PhotoMatchActivity extends Activity {
@@ -45,6 +51,8 @@ public class PhotoMatchActivity extends Activity {
     private static final String TAG = "PhotoMatchActivity";
     private ImageView imageView;
     private Bitmap photoRecup;
+
+    private static final int DISTANCE_MAX= 60;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,68 +64,102 @@ public class PhotoMatchActivity extends Activity {
         if(extra != null){
             Uri photoUri = (Uri)extra.get("uriFound");
             try {
-//                this.photoRecup = MediaStore.Images.Media.getBitmap(this.getContentResolver(), photoUri);
-                this.photoRecup = BitmapFactory.decodeFile(photoUri.getPath());
-                this.imageView = (ImageView)findViewById(R.id.photoRecup);
-//                this.imageView.setImageBitmap(this.photoRecup);
+                this.photoRecup = MediaStore.Images.Media.getBitmap(this.getContentResolver(), photoUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+//            this.photoRecup = BitmapFactory.decodeFile(photoUri.getPath());
+            this.imageView = (ImageView)findViewById(R.id.photoRecup);
+            this.imageView.setImageBitmap(this.photoRecup);
 
+            Log.d("path", photoUri.getPath());
 
-                OpenCVLoader.initDebug();
-                Mat matRecup = Highgui.imread(photoUri.getPath(), Highgui.IMREAD_GRAYSCALE);
+            OpenCVLoader.initDebug();
+//            Mat matRecup = Highgui.imread(photoUri.getPath(), Highgui.IMREAD_GRAYSCALE);
 
-                // test passage en gris
+            Mat matRecup = new Mat();
+            Utils.bitmapToMat(photoRecup, matRecup);
+
+            // test passage en gris
 //                Bitmap bmp = null;
 //                Utils.matToBitmap(matRecup, bmp);
 //                this.imageView.setImageBitmap(bmp);
 
-                Mat testDrawable = Highgui.imread("drawable://" + R.drawable.frame_18);
-//                Mat testDrawable = Highgui.imread("drawable://" + R.drawable.logo_bmw);
-//                Mat img = Utils.loadResource(this.getApplicationContext(), refrenceimgID, Highgui.CV_LOAD_IMAGE_COLOR);
+            // récup Mat depuis drawable
+            Mat m = new Mat();
+            try {
+                m = Utils.loadResource(this, R.drawable.frame_18, Highgui.CV_LOAD_IMAGE_GRAYSCALE);
+                Log.d("drawable", m.dump());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-                Log.e("testff", testDrawable.toString());
+            FeatureDetector detector = FeatureDetector.create(FeatureDetector.ORB);
+            DescriptorExtractor descriptor = DescriptorExtractor.create(DescriptorExtractor.ORB);
+            DescriptorMatcher matcher = DescriptorMatcher.create(DescriptorMatcher.BRUTEFORCE_HAMMING);
 
-                FeatureDetector detector = FeatureDetector.create(FeatureDetector.ORB);
-                DescriptorExtractor descriptor = DescriptorExtractor.create(DescriptorExtractor.ORB);
-                DescriptorMatcher matcher = DescriptorMatcher.create(DescriptorMatcher.BRUTEFORCE_HAMMING);
+            Mat descriptors1 = new Mat();
+            MatOfKeyPoint keypoints1 = new MatOfKeyPoint();
 
+            detector.detect(matRecup, keypoints1);
+            descriptor.compute(matRecup, keypoints1, descriptors1);
 
-                Mat descriptors1 = new Mat();
-                MatOfKeyPoint keypoints1 = new MatOfKeyPoint();
+            Mat descriptors2 = new Mat();
+            MatOfKeyPoint keypoints2 = new MatOfKeyPoint();
 
-                detector.detect(matRecup, keypoints1);
-                descriptor.compute(matRecup, keypoints1, descriptors1);
+            detector.detect(m, keypoints2);
+            descriptor.compute(m, keypoints2, descriptors2);
 
-                MatOfDMatch matches = new MatOfDMatch();
-                matcher.match(descriptors1, testDrawable, matches);
+            MatOfDMatch matches = new MatOfDMatch();
+            matcher.match(descriptors1, descriptors2, matches);
 
-                Log.e("dump", descriptors1.dump());
+            Log.d("matches", matches.dump());
 
-                File sdCard = Environment.getExternalStorageDirectory();
-                Toast.makeText(this.getApplicationContext(),
-                        sdCard.getAbsolutePath(),
-                        Toast.LENGTH_LONG).show(); // return false
+            List<DMatch> matchesList = matches.toList();
+            List<DMatch> matchesListFinal = new ArrayList<DMatch>();
 
-                File dir = new File (sdCard.getAbsolutePath() + "/opencv_perne_morin");
-                dir.mkdirs();
-                File file = new File(dir, "mat_store");
+            for(int i=0 ; i < matchesList.size() ; i++){
+                Log.d("matchesstring" ,matchesList.get(i).toString()); // donne DMatch [queryIdx=54, trainIdx=139, imgIdx=0, distance=54.0]
+                // queryIdx représente keypoints1
+                // trainIdx représente keypoints2
+                // distance à laquelle se trouve les deux points
 
-                FileOutputStream f = new FileOutputStream(file);
+                if(matchesList.get(i).distance <= this.DISTANCE_MAX){
+                    matchesListFinal.add(matchesList.get(i));
+                }
 
-                /***************** Serialisation marche pas ***********/
+            }
+
+            Toast.makeText(this.getApplicationContext(), String.valueOf(matchesListFinal.size()), Toast.LENGTH_LONG).show();
+
+            File sdCard = Environment.getExternalStorageDirectory();
+            Toast.makeText(this.getApplicationContext(),
+                    sdCard.getAbsolutePath(),
+                    Toast.LENGTH_LONG).show(); // return false
+
+            File dir = new File (sdCard.getAbsolutePath() + "/opencv_perne_morin");
+            dir.mkdirs();
+            File file = new File(dir, "mat_store.json");
+
+            FileOutputStream f = null;
+            try {
+                f = new FileOutputStream(file);
+                f.write(matToJson(descriptors1).getBytes());
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            /***************** Serialisation marche pas ***********/
 //                ObjectOutputStream os = new ObjectOutputStream(f);
 //                os.writeObject(matRecup);
 //                os.close();
 //                f.close();
 
-                Log.e(TAG,matToJson(matRecup));
-                f.write(matRecup.dump().getBytes());
-                f.write("hello".getBytes());
 
 
 
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
 
         }
         // todo : ajouter photo dans un coin pour rappeler la photo qu'on vient de capturer qu'on veut faire analyser
